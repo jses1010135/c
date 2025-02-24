a968c63a50c953455c7630c7e5b9fe7dc86fbce3
佳憲 鍾 <jses1010135@gmail.com>
2025-02-24 17:08:10 +0800

    1

--- list test.py
+++ list test.py
@@ -1,25 +1,29 @@
-mylist = [1,2,3,4,5] # 串列
-mylist.append(6) # 串列新增元素
-mylist.remove(6) # 串列刪除元素
-mylist.insert(0,0) # 串列插入元素
-mylist.pop(0) # 串列取出元素
-mylist.sort() # 串列排序
-mylist.reverse() # 串列反轉
-mytuple = (1,2,3,4,5) # 元組
-myset = {1,2,3,4,5} # 集合
-mydict = {'a':1,'b':2,'c':3} # 字典
+mylist = [] # 串列
+mylist.append([1,2,3,4,5])
+mylist.append([5,4,3,2,1])
+mylist.append([1,2,3,2,1])
+mylist.append([5,4,3,4,5])
+# mylist.extend([7,8,9,10,11]) # 串列擴展元素
 
-print (mylist[3])
-print (mylist[0])
-print (mylist[7])
+# mylist.remove(6) # 串列刪除元素
+# mylist.insert(0,0) # 串列插入元素
+# mylist.pop(0) # 串列取出元素
+# mylist.sort() # 串列排序
+# mylist.reverse() # 串列反轉
+# mytuple = (1,2,3,4,5) # 元組
+# myset = {1,2,3,4,5} # 集合
+# mydict = {'a':1,'b':2,'c':3} # 字典
+
+
 print (f"mylist={mylist}") # 串列
-print (f"mytuple={mytuple}") # 元組
-print (f"myset={myset}") # 集合
-print (f"mydict={mydict}") # 字典
-print (f"mylist[0]={mylist[0]}") # 串列索引
-print (f"mytuple[0]={mytuple[0]}") # 元組索引
-print (f"myset={myset}") # 集合
-print (f"mydict['a']={mydict['a']}") # 字典索引
-print (f"mydict.get('a')={mydict.get('a')}") # 字典索引
-print (f"mydict.get('d')={mydict.get('d')}") # 字典索引
-print (f"mydict.get('d','沒有這個鍵')={mydict.get('d','沒有這個鍵')}") # 字典索引
+print(mylist[1][3])
+# print (f"mytuple={mytuple}") # 元組
+# print (f"myset={myset}") # 集合
+# print (f"mydict={mydict}") # 字典
+# print (f"mylist[0]={mylist[0]}") # 串列索引
+# print (f"mytuple[0]={mytuple[0]}") # 元組索引
+# print (f"myset={myset}") # 集合
+# print (f"mydict['a']={mydict['a']}") # 字典索引
+# print (f"mydict.get('a')={mydict.get('a')}") # 字典索引
+# print (f"mydict.get('d')={mydict.get('d')}") # 字典索引
+# print (f"mydict.get('d','沒有這個鍵')={mydict.get('d','沒有這個鍵')}") # 字典索引
