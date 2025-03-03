ccb7e7926ef3357fd754a5192c9d93ef16213e4b
佳憲 鍾 <jses1010135@gmail.com>
2025-03-03 16:55:10 +0800

    1

--- 運算test.py
+++ 運算test.py
@@ -2,10 +2,39 @@ a=100
 b=200
 c=a+b
 c= a=b-3*a=5*b
+#算術運算子
 a%b#取餘數
 a//b#取商
 a**b#次方
 a**0.5#開根號
 a**(1/3)#開立方根
 a**-1#取倒數
+a**-2#取平方倒數
+a**-0.5#取根號倒數
+
+#比較運算子
+a==b#等於
+a!=b#不等於
+a>b#大於
+a<b#小於
+a>=b#大於等於
+a<=b#小於等於
+
+#邏輯運算子
+a is b#同一個物件
+a is not b#不是同一個物件
+a in b#在裡面
+a not in b#不在裡面
+
+#位元運算子
+a&b#and
+a|b#or
+a^b#xor
+a and b#and
+a or b#or
+not a#not
+~a#not
+a<<b#左移
+a>>b#右移
+
 print(c)
