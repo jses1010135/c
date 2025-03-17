98bfade940b465cd9e928ee91db572c17a617067
佳憲 鍾 <jses1010135@gmail.com>
2025-03-17 16:34:44 +0800

    修正compute

--- project[1113410005]/test2.py
+++ project[1113410005]/test2.py
@@ -1,12 +1,12 @@
 def coculate():
-    text=[0]*9
-    for i in range(9):
+    text=[0]*7
+    for i in range(7):
         text[i]=int(input(f"請輸入第{i+1}個整數: "))
     
     return text
 def compute(text):
-    text1=(text[1]*text[2]-text[3]*text[4]/text[5]*text[6]-text[7]*text[1])
-    text2=(text[1]*text[6]-text[3]*text[5]/text[4]*text[7]-text[5]*text[2])
+    text1=(text[1]*text[2]-text[3]*text[4]/text[5]*text[2]-text[6]*text[4])
+    text2=(text[1]*text[6]-text[3]*text[5]/text[4]*text[6]-text[5]*text[2])
     return text1,text2
 def main():
     text=coculate()
