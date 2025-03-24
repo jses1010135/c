fae9a628eb66012aab5c54430d18d6cfb4487273
佳憲 鍾 <jses1010135@gmail.com>
2025-03-24 17:08:38 +0800

    增加sort

--- test.py
+++ test.py
@@ -1,17 +1,14 @@
 def input_variables():
-    text = [0] * 5
-    for i in range(5):
-        text[i] = int(input(f"請輸入第{i+1}個整數: "))
+    text=[10]
+    text[0]=int(input("請輸入整數:"))
     return text
-
-def compute(text):
-    result = (text[1] * text[2] + text[3] / text[4] - text[1])
-    return result
-
+def conpute(text):
+    text[0]=text[1]*(text[2]+text[3])/text[4]-text[1]
+    return text[0]
 def main():
-    a = input_variables()
-    print(f"({a[1]}*({a[2]}+{a[3]})/{a[4]}-{a[1]})={compute(a)}")
-
+    a=input_variables()
+    print(f"{a[0]}*({a[1]}*({a[2]}+{a[3]})/{a[4]}-{a[1]})={conpute(a)}")
 if __name__ == "__main__":
     main()
 
+    
